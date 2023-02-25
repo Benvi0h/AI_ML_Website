43e5e93799f532c40687f51693d55e573c6197dc
Benvi0h <81503725+Benvi0h@users.noreply.github.com>
2023-02-25 17:58:33 +0530

    Blog_commit

--- Core_team.js
+++ Core_team.js
@@ -47,4 +47,23 @@ cards.forEach(function callback(card,index){
   }
 })
 
+const NavBar = document.querySelector(".navbar");
 
+document.addEventListener("scroll",Navbar_Scroll);
+
+function Navbar_Scroll()
+{
+  if(document.body.scrollTop > 130 ||
+    document.documentElement.scrollTop > 130)
+  { 
+    NavBar.style.height = "10px";
+    NavBar.style.boxShadow = "0 0 10px";
+    NavBar.style.opacity = "0";
+  }
+  else
+  {
+    NavBar.style.height = "10vh";
+    NavBar.style.boxShadow = "0 0 0px";
+    NavBar.style.opacity = "1";
+  }
+}
